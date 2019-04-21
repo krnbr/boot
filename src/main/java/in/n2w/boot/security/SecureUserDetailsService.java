8c7b31cc85594495a6f6b7aec983ef2378ca9da5
Karanbir Singh <krnbr@live.in>
2019-04-21 18:03:28 +0530

    user enabled field attached to the user details service

--- src/main/java/in/n2w/boot/security/SecureUserDetailsService.java
+++ src/main/java/in/n2w/boot/security/SecureUserDetailsService.java
@@ -36,7 +36,7 @@ public class SecureUserDetailsService implements UserDetailsService {
         if (user == null) {
             throw new UsernameNotFoundException("No user found with username: " + email);
         }
-        return new org.springframework.security.core.userdetails.User(user.getEmail(), passwordEncoder.encode(user.getPassword()), true, true, true, true, getAuthorities(ROLE_USER));
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), passwordEncoder.encode(user.getPassword()), user.getEnabled(), true, true, true, getAuthorities(ROLE_USER));
     }
 
     private Collection<? extends GrantedAuthority> getAuthorities(String role) {
