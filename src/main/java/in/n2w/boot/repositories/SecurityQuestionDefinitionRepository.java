d4d512e0d5795e060ba97169ce50c84f691b570d
Karanbir Singh <krnbr@live.in>
2019-04-22 13:32:05 +0530

    added security questions for password reset flow

---
+++ src/main/java/in/n2w/boot/repositories/SecurityQuestionDefinitionRepository.java
@@ -0,0 +1,8 @@
+package in.n2w.boot.repositories;
+
+import in.n2w.boot.entities.SecurityQuestionDefinition;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SecurityQuestionDefinitionRepository extends JpaRepository<SecurityQuestionDefinition, Long> {
+
+}
--- src/main/java/in/n2w/boot/web/controllers/RegoController.java
+++ src/main/java/in/n2w/boot/web/controllers/RegoController.java
@@ -2,10 +2,13 @@ package in.n2w.boot.web.controllers;
 
 import com.google.common.collect.ImmutableMap;
 import in.n2w.boot.entities.PasswordResetToken;
+import in.n2w.boot.entities.SecurityQuestion;
 import in.n2w.boot.entities.User;
 import in.n2w.boot.entities.VerificationToken;
 import in.n2w.boot.events.RegoCompleteEvent;
 import in.n2w.boot.exceptions.EmailExistsException;
+import in.n2w.boot.repositories.SecurityQuestionDefinitionRepository;
+import in.n2w.boot.repositories.SecurityQuestionRepository;
 import in.n2w.boot.security.SecureUserDetailsService;
 import in.n2w.boot.services.UserService;
 import org.slf4j.Logger;
@@ -32,6 +35,8 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 /**
@@ -57,32 +62,38 @@ public class RegoController {
     @Autowired
     private Environment env;
 
+    @Autowired
+    SecurityQuestionDefinitionRepository securityQuestionDefinitionRepository;
+
+    @Autowired
+    SecurityQuestionRepository securityQuestionRepository;
+
     @RequestMapping(value = "signup")
     public ModelAndView registrationForm() {
-        return new ModelAndView("register", "user", new User());
+        final Map<String, Object> model = new HashMap<>();
+        model.put("user", new User());
+        model.put("questions", securityQuestionDefinitionRepository.findAll());
+        return new ModelAndView("register", model);
     }
 
     @RequestMapping(value = "sign-up")
-    public ModelAndView registerUser(@Valid final User user, final BindingResult result, final HttpServletRequest request) {
+    public ModelAndView registerUser(@Valid final User user, final @RequestParam Long questionId, @RequestParam final String answer, final BindingResult result, final HttpServletRequest request, final RedirectAttributes redirectAttributes) {
         if (result.hasErrors()) {
             return new ModelAndView("register", "user", user);
         }
         try {
-            final User registeredUser = userService.registerNewUser(user);
+            final User registered = userService.registerNewUser(user);
 
-            // TODO this id for dev only but eventually there should be better way out for this
+            securityQuestionDefinitionRepository.findById(questionId).ifPresent(questionDefinition -> securityQuestionRepository.
+                    save(new SecurityQuestion(user, questionDefinition, answer)));
 
-            final String appUrl =
-                    request.getScheme() + "://" +
-                    request.getServerName() + ":" +
-                    request.getServerPort() +
-                    request.getContextPath();
-            logger.info("appUrl "+appUrl);
-            eventPublisher.publishEvent(new RegoCompleteEvent(registeredUser, appUrl));
+            final String appUrl = "http://" + request.getServerName() + ":" + request.getServerPort() + request.getContextPath();
+            eventPublisher.publishEvent(new RegoCompleteEvent(registered, appUrl));
         } catch (EmailExistsException e) {
             result.addError(new FieldError("user", "email", e.getMessage()));
-            return new ModelAndView("register", "user", user);
+            return new ModelAndView("registrationPage", "user", user);
         }
+        redirectAttributes.addFlashAttribute("message", "You should receive a confirmation email shortly");
         return new ModelAndView("redirect:/login");
     }
 
@@ -145,16 +156,26 @@ public class RegoController {
         final Authentication auth = new UsernamePasswordAuthenticationToken(user, null, userDetailsService.loadUserByUsername(user.getEmail()).getAuthorities());
         SecurityContextHolder.getContext().setAuthentication(auth);
 
-        return new ModelAndView("reset-password");
+        return new ModelAndView("reset-password", ImmutableMap.of("questions", securityQuestionDefinitionRepository.findAll()));
     }
 
     @RequestMapping(value = "/user/save-password", method = RequestMethod.POST)
     @ResponseBody
-    public ModelAndView savePassword(@RequestParam("password") final String password, @RequestParam("passwordConfirmation") final String passwordConfirmation, final RedirectAttributes redirectAttributes) {
+    public ModelAndView savePassword(@RequestParam("password") final String password, @RequestParam("passwordConfirmation") final String passwordConfirmation, @RequestParam final Long questionId, @RequestParam final String answer,
+                                     final RedirectAttributes redirectAttributes) {
         if (!password.equals(passwordConfirmation)) {
-            return new ModelAndView("reset-password", ImmutableMap.of("errorMessage", "Passwords do not match"));
+            final Map<String, Object> model = new HashMap<>();
+            model.put("errorMessage", "Passwords do not match");
+            model.put("questions", securityQuestionDefinitionRepository.findAll());
+            return new ModelAndView("reset-password", model);
         }
         final User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (securityQuestionRepository.findByQuestionDefinitionIdAndUserIdAndAnswer(questionId, user.getId(), answer) == null) {
+            final Map<String, Object> model = new HashMap<>();
+            model.put("errorMessage", "Answer to security question is incorrect");
+            model.put("questions", securityQuestionDefinitionRepository.findAll());
+            return new ModelAndView("reset-password", model);
+        }
         userService.changeUserPassword(user, password);
         redirectAttributes.addFlashAttribute("message", "Password reset successfully");
         return new ModelAndView("redirect:/login");
@@ -162,6 +183,9 @@ public class RegoController {
 
     private SimpleMailMessage constructResetTokenEmail(final String contextPath, final String token, final User user) {
         final String url = contextPath + "/user/change-password?id=" + user.getId() + "&token=" + token;
+
+        logger.info("url change-password -> "+url);
+
         final SimpleMailMessage email = new SimpleMailMessage();
         email.setTo(user.getEmail());
         email.setSubject("Reset Password");
